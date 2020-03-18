efd294a24ae59e01a2c7c40faa8973bd4caf63c4
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-18 22:04:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
-def square_array(numbers)
-new_numbers = []
-end 	  numbers.each do |num|
-    new_numbers << (num ** 2)
-  end
-  return new_numbers
+def square_array(array)
+   new_array = []
+   array.each do |array|
+    new_array << (array ** 2)
+   end
+   return new_array
 end
